9424cff19836d1361b42dd76bb12bf56a85a7260
Your Name <wallicestenewaweru@gmail.com>
2023-09-19 16:13:15 +0300

    dotenv

--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
 // initialisizing the app
+const app = express();
 
-const app = express()
+// middlewares
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express(JSON));
+require("dotenv").config();
 
-
-// middlewares 
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express(JSON))
-require('dotenv').config();
-
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
