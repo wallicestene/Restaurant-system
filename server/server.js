5b36def6a4eb9e65e65fc40037c7d996792a2c69
Your Name <wallicestenewaweru@gmail.com>
2023-09-19 16:09:52 +0300

    done

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+// initialisizing the app
 
 const app = express()
+
+
+// middlewares 
+app.use(morgan("dev"))
+app.use(cors())
+app.use(express(JSON))
+require('dotenv').config();
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
