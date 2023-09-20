f8ac66839cbeb3dc1c747660473dabd834061882
Your Name <wallicestenewaweru@gmail.com>
2023-09-20 22:07:07 +0300

    connection to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // initialisizing the app
 const app = express();
@@ -9,13 +11,29 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
-require("dotenv").config();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+// connecting to mongoDB Database
+mongoose
+  .connect("mongodb://0.0.0.0:27017/restaurant-system", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+      console.log("Connected to the database");
+    });
+  })
+  .catch((err) => {
+    console.error("Error while trying to connect", err.message);
+  });
 
+// fallback route for handling unknown routes
 
-app.use((req,res) => {
-  res.status(404).json({message: "Route not found!"})
+app.get("/all", (req,res) => {
+  return res.send('Hello World');
 })
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found!" });
+});
