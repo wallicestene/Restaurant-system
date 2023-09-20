baaa3c3fefe47c0199ce9fd7c5bb7b83d822c7cf
Your Name <wallicestenewaweru@gmail.com>
2023-09-20 21:33:21 +0300

    setting up server

--- server/server.js
+++ server/server.js
@@ -7,10 +7,15 @@ const app = express();
 
 // middlewares
 app.use(morgan("dev"));
+app.use(express.json());
 app.use(cors());
-app.use(express(JSON));
 require("dotenv").config();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
+
+
+app.use((req,res) => {
+  res.status(404).json({message: "Route not found!"})
+})
