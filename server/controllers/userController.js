df4b16b50a0ea3c10c8ec62fbcd02b5e40257cc7
Your Name <wallicestenewaweru@gmail.com>
2023-09-21 22:13:43 +0300

    user sign in is ready

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,22 @@
-const User = require("../models/userModel")
-const mongoose = require("mongoose")
+const User = require("../models/userModel");
+const mongoose = require("mongoose");
 
 // LogIn
-const login = (req,res) => {
-    res.json({mssg: "login user"})
-}
-const signup = (req,res) => {
-    res.json({mssg: "signup user"})
-}
+const loginUser = (req, res) => {
+  res.json({ mssg: "login user" });
+};
+const signupUser = (req, res) => {
+  const { email, password } = req.body;
+  User.signup(email, password)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(400).json({ error: error.message });
+    });
+};
 
 module.exports = {
-    login,
-    signup
-}
+  loginUser,
+  signupUser,
+};
