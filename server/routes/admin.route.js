68c20c2d4caecbcb8e17f478a7969aeb87d2e623
Your Name <wallicestenewaweru@gmail.com>
2023-09-23 13:51:03 +0300

    restaurant routes/set

--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -1,15 +1,30 @@
-const AdminBro = require('admin-bro')
-const AdminBroExpress = require('@admin-bro/express')
-const AdminBroMongoose = require('@admin-bro/mongoose')
+const AdminBro = require("admin-bro");
+const AdminBroExpress = require("@admin-bro/express");
+const AdminBroMongoose = require("@admin-bro/mongoose");
 
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-AdminBro.registerAdapter(AdminBroMongoose)
+AdminBro.registerAdapter(AdminBroMongoose);
+
 const adminBro = new AdminBro({
   databases: [mongoose],
-  rootPath: '/admin',
-})
+  rootPath: "/admin",
+});
 
-const router = AdminBroExpress.buildRouter(adminBro)
+const ADMIN = {
+  email: process.env.ADMIN_EMAIL || "wallicestenewaweru@gmail.com",
+  password: process.env.ADMIN_PASSWORD || "wallace"
+}
+const router = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
+  cookieName: process.env.ADMIN_COOKIE_NAME || "admin-bro",
+  cookiePassword: process.env.ADMIN_COOKIE_PASS || "supersecret-and-long-password-for-a-cookie",
+  authenticate: async (email, password) => {
+    if(email === ADMIN.email && password === ADMIN.password){
+      return ADMIN
+    }
 
-module.exports = router
+    return null
+  }
+});
+
+module.exports = router;
