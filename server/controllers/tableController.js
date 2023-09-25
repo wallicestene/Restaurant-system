38809c304fd9307b9ca47715c480a005bd838b38
Your Name <wallicestenewaweru@gmail.com>
2023-09-25 14:55:28 +0300

    tableController functions

--- server/controllers/tableController.js
+++ server/controllers/tableController.js
@@ -4,14 +4,20 @@ const Table = require("../models/tableModel");
 // add a table
 const addTable = (req, res) => {
   const { restaurant_id, number, capacity } = req.body;
-  Table.create({ restaurant_id, number, capacity }).then((result) => {
-    res.status(200).json(result);
-  });
-  
+  Table.create({ restaurant_id, number, capacity })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({
+          error: `Error occured while adding tables to database ${err}`,
+        });
+    });
 };
 
 // get all tables in specific restaurant
-
 const getTablesByRestaurantId = (req, res) => {
   let { id } = req.params;
   if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -20,12 +26,17 @@ const getTablesByRestaurantId = (req, res) => {
   let query = { restaurant_id: id };
   Table.find(query)
     .then((tables) => {
+      if (!tables) {
+        return res.status(404).json("no tables were found");
+      }
       res.status(200).json(tables);
     })
-    .catch(err => {
-      res.status(500).json(
-       {error: `Error occured while retrieving tables from database ${err}`}
-      )
+    .catch((err) => {
+      res
+        .status(500)
+        .json({
+          error: `Error occured while retrieving tables from database ${err}`,
+        });
     });
 };
 
