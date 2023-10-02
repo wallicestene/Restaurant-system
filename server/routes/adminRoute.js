6dc1aae570f39d48cb83088309f84f2ca7c3c205
Your Name <wallicestenewaweru@gmail.com>
2023-10-02 15:41:47 +0300

    added reference to the adminbro

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -21,27 +21,49 @@ const adminBro = new AdminBro({
     },
     {
       resource: Table,
+      options: {
+        properties: {
+          restaurantId: {
+            reference: "restaurant",
+          },
+        },
+      },
     },
     {
       resource: Reservation,
+      options:{
+        properties: {
+          userId: {
+            reference: "user",
+          },
+          tableId: {
+            reference: "table",
+          },
+          restaurantId: {
+            reference: "restaurant",
+          },
+        },
+      }
     },
   ],
 });
 
 const ADMIN = {
   email: process.env.ADMIN_EMAIL || "wallicestenewaweru@gmail.com",
-  password: process.env.ADMIN_PASSWORD || "wallace"
-}
+  password: process.env.ADMIN_PASSWORD || "wallace",
+};
 const router = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
   cookieName: process.env.ADMIN_COOKIE_NAME || "admin-bro",
-  cookiePassword: process.env.ADMIN_COOKIE_PASS || "supersecret-and-long-password-for-a-cookie",
+  cookiePassword:
+    process.env.ADMIN_COOKIE_PASS ||
+    "supersecret-and-long-password-for-a-cookie",
   authenticate: async (email, password) => {
-    if(email === ADMIN.email && password === ADMIN.password){
-      return ADMIN
+    if (email === ADMIN.email && password === ADMIN.password) {
+      return ADMIN;
     }
 
-    return null
-  }
+    return null;
+  },
 });
 
 module.exports = router;
