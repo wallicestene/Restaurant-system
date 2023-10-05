a07cc2d6077326b9a48616f0a6704d1d24564aac
Your Name <wallicestenewaweru@gmail.com>
2023-10-05 18:49:01 +0300

    navbarMobile

---
+++ client/src/components/NavbarMobile.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function NavbarMobile() {
+  return (
+    <div>
+        <nav>
+        <ul className="flex flex-col items-center gap-10 ">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Hotels</a>
+            </li>
+            <li>
+              <a href="/">Hotels</a>
+            </li>
+            <li>
+              <a href="/">Hotels</a>
+            </li>
+          </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default NavbarMobile
