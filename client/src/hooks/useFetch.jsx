3a0ed54b6c133f9bd1876c931517ada1f7c21a69
Your Name <wallicestenewaweru@gmail.com>
2023-10-07 20:45:48 +0300

    added a custome hook/fetching data

---
+++ client/src/hooks/useFetch.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetch(url)
+        .then((response) => {
+          if (!response.ok) {
+            throw Error("Failed to fetch data");
+          } else {
+            return response.json();
+          }
+        })
+        .then((result) => {
+          setData(result);
+          setIsloading(false);
+        })
+        .catch((error) => {
+          setError(error.message);
+          setIsloading(false);
+        });
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, error };
+};
+
+export default useFetch;
