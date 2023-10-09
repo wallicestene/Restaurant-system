5b6ef047f33db0c55dee79f3a40f68d02080dbc6
Your Name <wallicestenewaweru@gmail.com>
2023-10-09 23:26:44 +0300

    last commit

--- client/src/pages/RestaurantDetailsPage.jsx
+++ client/src/pages/RestaurantDetailsPage.jsx
@@ -10,18 +10,20 @@ const RestaurantDetailsPage = () => {
   return (
     <section className="h-screen w-11/12 mx-auto">
       <div className="pt-12 pb-2">
-        <h1 className=" font-bold font-Montserrat text-lg text-totem-pole-600">{data.name}</h1>
+        <h1 className=" font-bold font-Montserrat text-lg text-totem-pole-600">
+          {data?.name}
+        </h1>
       </div>
       <div className="grid grid-cols-2 w-11/12 mx-auto gap-4 rounded-xl overflow-hidden h-72 ">
         <div className=" h-72 relative">
           <img
-            src={data.images[0]}
+            src={data?.images[0]}
             className="absolute inset-0 w-full h-full object-cover"
             alt=""
           />
         </div>
         <div className="h-72  grid grid-cols-2 gap-2">
-          {data.images.slice(1, 5).map((image, index) => (
+          {data?.images.slice(1, 5).map((image, index) => (
             <div
               key={index}
               className="h-36 w-full relative overflow-hidd rou"
