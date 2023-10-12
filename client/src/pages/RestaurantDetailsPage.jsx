b266c20ad5bf7392bb657ccf6774b0020da70b97
Your Name <wallicestenewaweru@gmail.com>
2023-10-12 12:02:05 +0300

    restaurant mobile view

--- client/src/pages/RestaurantDetailsPage.jsx
+++ client/src/pages/RestaurantDetailsPage.jsx
@@ -37,63 +37,74 @@ const RestaurantDetailsPage = () => {
     getTables();
   }, [data]);
   return (
-    <section className="h-screen w-11/12 mx-auto">
+    <section className="h-screen w-11/12 mx-auto flex flex-col justify-center">
       {error && <p>{error}</p>}
       {isLoading && <p>Loading....</p>}
       {!isLoading && (
         <div>
-          <div className="pt-12 pb-2">
+          <div className="">
             <h1 className=" font-bold font-Montserrat text-lg text-totem-pole-600">
               {data?.name}
             </h1>
           </div>
-          <div className=" grid grid-cols-2 place-items-center p-1">
+          <div className=" grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-5 place-items-center p-1">
             <div className=" flex flex-col items-center flex-wrap gap-3 w-full justify-center ">
-              <div className=" h-64 w-96 rounded-xl overflow-hidden ">
+              <div className=" h-64 lg:w-96 md:w-96 w-full rounded-xl overflow-hidden ">
                 <img
                   src={data.images[value]}
                   className=" h-full w-full object-cover"
                   alt={data.name}
                 />
               </div>
-              <div className=" flex flex-row flex-wrap gap-3 ">
+              <div className=" flex flex-row flex-wrap justify-center gap-3 w-full">
                 {data.images.map((image, index) => (
                   <div
                     key={index}
-                    className={` h-16 w-16 rounded-lg overflow-hidden ${
-                      value == index && " border-2 border-totem-pole-500 "
+                    className={` lg:h-16 lg:w-16 md:h-16 md:w-16 lg:rounded-lg md:rounded-lg overflow-hidden ${
+                      value == index && " lg:border-2 md:border-2 border-totem-pole-500 "
                     }`}
                   >
                     <img
                       src={image}
                       onClick={() => setValue(index)}
-                      className={` h-full w-full hover:cursor-pointer object-cover ${
+                      className={` lg:flex md:flex hidden  h-full w-full hover:cursor-pointer object-cover  ${
                         value == index && " opacity-60"
                       }`}
                       alt=""
                     />
+                    <div onClick={() => setValue(index)} className={` border h-4 w-4 border-totem-pole-500 rounded-full cursor-pointer ${
+                        value == index && " bg-totem-pole-500"
+                      }`}/>
                   </div>
                 ))}
               </div>
             </div>
-            <div className=" h-full w-full border border-totem-pole-400 px-2 font-poppins">
+            <div className=" h-full w-full border border-totem-pole-400 px-2 font-poppins py-2">
               <div className=" bg-totem-pole-400 inline-block p-1 rounded-md text-totem-pole-100 my-1">
-                <h1 >Tables</h1>
+                <h1>Tables</h1>
               </div>
               {tableError && <p>{error}</p>}
               {loading && <p>Loading...</p>}
-              {!loading && (
+              {!loading && tables.length > 0 && (
                 <div className="flex font-Montserrat items-center gap-5 overflow-x-scroll px-2 ">
                   {tables.map((table, index) => (
-                    <button disabled={table.occupied && "true"}  key={index} className={` relative bg-green-800 flex-shrink-0  text-sm text-totem-pole-100 justify-center h-20 w-28 flex flex-col items-center px-1 rounded-md ${table.occupied && " bg-red-700 "} overflow-hidden`}>
-                       <span>0{table.number}</span>
+                    <button
+                      disabled={table.occupied}
+                      key={index}
+                      className={` relative bg-green-800 flex-shrink-0  text-sm text-totem-pole-100 justify-center h-20 w-28 flex flex-col items-center px-1 rounded-md ${
+                        table.occupied && " bg-red-700 "
+                      } overflow-hidden`}
+                    >
+                      <span>0{table.number}</span>
                       <TableRestaurant />
                       <span>Guests: {table.capacity}</span>
-                      { table.occupied &&
-                      <div className=" absolute top-0 bottom-0 h-full w-full grid place-items-center bg-red-600 bg-opacity-50">
-                        <span className=" border border-totem-pole-100 px-2">Reserved</span>
-                      </div>
-                      }
+                      {table.occupied && (
+                        <div className=" absolute top-0 bottom-0 h-full w-full grid place-items-center bg-red-600 bg-opacity-50">
+                          <span className=" border border-totem-pole-100 px-2">
+                            Reserved
+                          </span>
+                        </div>
+                      )}
                     </button>
                   ))}
                 </div>
