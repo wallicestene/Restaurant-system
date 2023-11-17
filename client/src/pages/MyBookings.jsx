ec195d8788543f9b325b3d5eaedbd7de647d27af
Your Name <wallicestenewaweru@gmail.com>
2023-11-17 19:19:30 +0300

    update

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useUserContext } from "../hooks/Usercontext";
 import Bookings from "../components/Bookings";
+import { Skeleton } from "@mui/material";
 
 // eslint-disable-next-line react/prop-types
 const MyBookings = ({ subPage }) => {
@@ -9,6 +10,7 @@ const MyBookings = ({ subPage }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [{ user }] = useUserContext();
+  const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   useEffect(() => {
     const getMyBookings = () => {
       fetch(`http://localhost:3000/api/reservations/?userId=${user?.userId}`, {
@@ -42,7 +44,73 @@ const MyBookings = ({ subPage }) => {
   return (
     <>
       <div className=" lg:w-1/2 w-full">
-        {loading && <p>Loading...</p>}
+        <div className=" flex flex-col gap-y-2">
+          {loading &&
+            skeleton.map((skeleton, index) => (
+              <div key={index} className=" bg-gray-200/90 rounded">
+                <div className=" flex gap-2">
+                  <Skeleton
+                    animation="wave"
+                    variant="rounded"
+                    width="50%"
+                    height={120}
+                    sx={{
+                      bgcolor: "grey.500",
+                    }}
+                  />
+                  <div className=" w-full flex flex-col justify-between py-3 ">
+                    <Skeleton
+                      animation="wave"
+                      variant="rounded"
+                      width="80%"
+                      height={22}
+                      sx={{
+                        bgcolor: "grey.500",
+                      }}
+                    />
+                    <div className=" flex justify-around gap-x-2 px-2">
+                      <Skeleton
+                        animation="wave"
+                        variant="rounded"
+                        width="50%"
+                        height={22}
+                        sx={{
+                          bgcolor: "grey.500",
+                        }}
+                      />
+                      <Skeleton
+                        animation="wave"
+                        variant="rounded"
+                        width="50%"
+                        height={22}
+                        sx={{
+                          bgcolor: "grey.500",
+                        }}
+                      />
+                      <Skeleton
+                        animation="wave"
+                        variant="rounded"
+                        width="50%"
+                        height={22}
+                        sx={{
+                          bgcolor: "grey.500",
+                        }}
+                      />
+                    </div>
+                    <Skeleton
+                      animation="wave"
+                      variant="rounded"
+                      width="50%"
+                      height={22}
+                      sx={{
+                        bgcolor: "grey.500",
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
         {error && <p>{error}</p>}
         {!loading && myBookings.length > 0 && (
           <div className=" flex flex-col gap-y-2 ">
