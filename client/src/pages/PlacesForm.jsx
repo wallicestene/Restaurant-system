09559fbaae23127172020f75ae8dc3c0eecfcea1
Your Name <wallicestenewaweru@gmail.com>
2024-01-11 19:42:24 +0300

    fourth commit

--- client/src/pages/PlacesForm.jsx
+++ client/src/pages/PlacesForm.jsx
@@ -115,7 +115,8 @@ const PlacesForm = () => {
   }
 
   return (
-    <div className="w-full lg:w-11/12 mx-auto font-mulish py-20 px-2 text-">
+    <div className=" flex flex-col justify-center py-20">
+      {" "}
       <button
         className=" flex items-center text-sm hover:bg-totem-pole-100 w-fit py-1 px-2 rounded-md transition-colors delay-150 duration-300"
         onClick={() => navigate(-1)}
@@ -129,138 +130,153 @@ const PlacesForm = () => {
         </span>
         <span>Back</span>
       </button>
-      <div className="w-full px-2">
-        <form>
-          {currentPage === 0 && (
-            <>
-              {inputTitle(
-                "Title",
-                "Title for your restaurant. Should be short and precise"
-              )}
-              <input
-                type="text"
-                className=""
-                placeholder="title, for example: My Restaurant "
-                name="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </>
-          )}
-          {currentPage === 1 && (
-            <>
-              {inputTitle("Address", "Address to your restaurant")}
-              <input
-                type="text"
-                className=""
-                placeholder="Address e.g Nairobi,Kenya"
-                name="address"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-              />
-            </>
-          )}
-          {currentPage === 2 && (
-            <>
-              {inputTitle("Images", "The more the images the better")}
-              <ImagesUploader
-                images={images}
-                setImages={setImages}
-                imageLink={imageLink}
-                setImageLink={setImageLink}
-              />
-            </>
-          )}
-          {currentPage === 3 && (
-            <>
-              {inputTitle("Description", "The description of your place")}
-              <textarea
-                className=" border border-totem-pole-300 w-full py-2 indent-2 outline-none rounded-md h-36"
-                name="description"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              ></textarea>
-            </>
-          )}
-          {currentPage === 4 && (
-            <>
-              {inputTitle(
-                "Place to sleep",
-                "The place to sleep i.e - bedrooms and the sleeping position"
-              )}
-              <MenuItems
-                bedroom={bedroom}
-                setBedroom={setBedroom}
-                sleepingPosition={sleepingPosition}
-                setSleepingPosition={setSleepingPosition}
-                whereToSleep={whereToSleep}
-                setWhereToSleep={setWhereToSleep}
-              />
-            </>
-          )}
-          {currentPage === 5 && (
-            <>
-              {inputTitle(
-                "Guests",
-                "The number of guests this place needs e.g 2 adults, 1 child, 1 infant..."
-              )}
-              <input
-                type="number"
-                className=""
-                placeholder="Nymber of Guests"
-                name="guests"
-                value={guests}
-                min={1}
-                onChange={(e) => setGuests(e.target.value)}
-              />
-            </>
-          )}
-          {currentPage === 6 && (
-            <>
-              {inputTitle("Price", "The price of your place i.e $ 54")}
-              <input
-                type="number"
-                className=""
-                placeholder="Price eg $ 54"
-                name="price"
-                value={price}
-                min={10}
-                onChange={(e) => setPrice(e.target.value)}
-              />
-            </>
-          )}
-          {currentPage === 7 && (
-            <>
-              {inputTitle(
-                "Tags",
-                "Tags, for example dates, fast-food, five-star..."
-              )}
-              <Tags selectedTags={tags} setSelectedTags={setTags} />
-              {inputTitle("Amenities", "Amenities in your place")}
-              <Amenities
-                selectedAmenities={amenities}
-                setSelectedAmenities={setAmenities}
-              />
-            </>
-          )}
-          {currentPage === 8 && (
-            <div className=" my-4 flex items-center justify-center border p-2 rounded">
-              <button
-                onClick={saveRestaurant}
-                className=" lg:w-1/2 w-full bg-green-700 text-totem-pole-50 py-2 text-center rounded-md"
-              >
-                Save
-              </button>
-            </div>
-          )}
-        </form>
-        <button
-          disabled={currentPage === totalPages}
-          onClick={() => setcurrentPage((prevValue) => prevValue + 1)}
-          className={` bg-green-600 py-2 px-5 rounded-lg text-white`}
-        >
-          Next
-        </button>
+      <div className="w-full lg:w-3/5 mx-auto font-mulish border h-screen overflow-scroll border-black grid place-items-center p-4">
+        <div className="w-full h-full">
+          <form>
+            {currentPage === 0 && (
+              <>
+                {inputTitle(
+                  "Title",
+                  "Title for your restaurant. Should be short and precise"
+                )}
+                <input
+                  type="text"
+                  className=""
+                  placeholder="title, for example: My Restaurant "
+                  name="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </>
+            )}
+            {currentPage === 1 && (
+              <>
+                {inputTitle("Address", "Address to your restaurant")}
+                <input
+                  type="text"
+                  className=""
+                  placeholder="Address e.g Nairobi,Kenya"
+                  name="address"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+              </>
+            )}
+            {currentPage === 2 && (
+              <>
+                {inputTitle("Images", "The more the images the better")}
+                <ImagesUploader
+                  images={images}
+                  setImages={setImages}
+                  imageLink={imageLink}
+                  setImageLink={setImageLink}
+                />
+              </>
+            )}
+            {currentPage === 3 && (
+              <>
+                {inputTitle("Description", "The description of your place")}
+                <textarea
+                  className=" border border-totem-pole-300 w-full py-2 indent-2 outline-none rounded-md h-36"
+                  name="description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+              </>
+            )}
+            {currentPage === 4 && (
+              <>
+                {inputTitle(
+                  "Place to sleep",
+                  "The place to sleep i.e - bedrooms and the sleeping position"
+                )}
+                <MenuItems
+                  bedroom={bedroom}
+                  setBedroom={setBedroom}
+                  sleepingPosition={sleepingPosition}
+                  setSleepingPosition={setSleepingPosition}
+                  whereToSleep={whereToSleep}
+                  setWhereToSleep={setWhereToSleep}
+                />
+              </>
+            )}
+            {currentPage === 5 && (
+              <>
+                {inputTitle(
+                  "Guests",
+                  "The number of guests this place needs e.g 2 adults, 1 child, 1 infant..."
+                )}
+                <input
+                  type="number"
+                  className=""
+                  placeholder="Nymber of Guests"
+                  name="guests"
+                  value={guests}
+                  min={1}
+                  onChange={(e) => setGuests(e.target.value)}
+                />
+              </>
+            )}
+            {currentPage === 6 && (
+              <>
+                {inputTitle("Price", "The price of your place i.e $ 54")}
+                <input
+                  type="number"
+                  className=""
+                  placeholder="Price eg $ 54"
+                  name="price"
+                  value={price}
+                  min={10}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </>
+            )}
+            {currentPage === 7 && (
+              <>
+                {inputTitle(
+                  "Tags",
+                  "Tags, for example dates, fast-food, five-star..."
+                )}
+                <Tags selectedTags={tags} setSelectedTags={setTags} />
+                {inputTitle("Amenities", "Amenities in your place")}
+                <Amenities
+                  selectedAmenities={amenities}
+                  setSelectedAmenities={setAmenities}
+                />
+              </>
+            )}
+            {currentPage === 8 && (
+              <div className=" my-4 flex items-center justify-center border p-2 rounded">
+                <button
+                  onClick={saveRestaurant}
+                  className=" lg:w-1/2 w-full bg-green-700 text-totem-pole-50 py-2 text-center rounded-md"
+                >
+                  Save
+                </button>
+              </div>
+            )}
+          </form>
+        </div>
+        <div className="  flex justify-between w-11/12 mx-auto">
+          <button
+            disabled={currentPage === 0}
+            onClick={() =>
+              setcurrentPage((prevValue) =>
+                prevValue <= 0 ? 0 : prevValue - 1
+              )
+            }
+            className={` bg-red-600 py-2 px-5 rounded-lg text-white`}
+          >
+            Back
+          </button>
+          <button
+            disabled={currentPage === totalPages}
+            onClick={() => setcurrentPage((prevValue) => prevValue + 1)}
+            className={` bg-green-600 py-2 px-5 rounded-lg text-white`}
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
