0dde10adefe08e5e8ef99e9ff30e3852805d9e88
Your Name <wallicestenewaweru@gmail.com>
2024-02-10 14:38:40 +0300

    footer update2

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,17 +2,20 @@ import {
   Favorite,
   GitHub,
   Instagram,
+  KeyboardArrowDown,
   Twitter,
   YouTube,
 } from "@mui/icons-material";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const [showFooter, setShowfooter] = useState(false);
   return (
-    <footer className="text-ebony-50 py-5 px-1 lg:px-8  font-mulish border rounded-t-md">
-      <div className=" flex items-center justify-between">
+    <footer className={`fixed bottom-0 left-0 z-10 w-full px-2 bg-white text-ebony-50 font-mulish border rounded-t-md transition-all duration-300 ease-linear h-10 ${showFooter && " h-[250px] "}`}>
+      <div className=" flex items-center justify-between flex-wrap h-10">
         <div className=" lg:text-sm text-xs text-neutral-500 grid place-items-center">
-          <p className=" my-1">
+          <p>
             Made with{" "}
             <span className=" text-red-600">
               <Favorite fontSize="small" />
@@ -29,7 +32,8 @@ const Footer = () => {
             All rights reserved
           </p>
         </div>
-        <div className="flex items-center justify-center gap-3 my-5 text-neutral-400">
+
+        <div className="flex items-center justify-center gap-3 text-neutral-400">
           <a href="https://github.com/wallicestene">
             <div className=" h-8 w-8 grid place-items-center border border-ebony-50 rounded-full">
               <GitHub fontSize="small" />
@@ -51,61 +55,68 @@ const Footer = () => {
             </div>
           </a>
         </div>
-      </div>
-      <div className=" font-thin lg:grid lg:grid-cols-4 grid-cols-2 lg:gap-5 gap-8 place-items-center flex flex-row flex-wrap">
         <div>
-          <h1 className=" text-lg my-1 font-semibold text-totem-pole-600">
-            Bookify
-          </h1>
-          <div className=" lg:text-sm text-xs  text-neutral-500 flex flex-col gap-2 overflow-ellipsis">
-            <p>
-              <strong className=" text-ebony-50">Address: </strong> 123
-              street,Embakasi, KE, 12345 KENYA
-            </p>
-            <p>
-              <strong className=" text-ebony-50">Phone: </strong>+254 792 8174
-              28
-            </p>
-            <p>
-              <strong className=" text-ebony-50">Email: </strong>
-              wallicestenewaweru@gmail.com
-            </p>
-          </div>
-        </div>
-        <div>
-          <h1 className="  text-lg text-ebony-50 my-1 first-letter:uppercase font-semibold">
-            Company
-          </h1>
-          <div className=" lg:text-sm text-xs text-neutral-500 flex flex-col gap-2">
-            <a href="#">About</a>
-            <a href="#">Careers</a>
-            <a href="#">Blog</a>
-            <a href="#">Terms & Policy</a>
-          </div>
-        </div>
-        <div>
-          <h1 className=" text-lg text-ebony-50 my-1 first-letter:uppercase font-semibold">
-            Discover
-          </h1>
-          <div className="lg:text-sm text-xs text-neutral-500 flex flex-col gap-2">
-            <a href="#">Trust & Safety</a>
-            <a href="#">Travel Credit</a>
-            <a href="#">Gift Cards</a>
-            <a href="#">Bookify Picks</a>
-          </div>
-        </div>
-        <div>
-          <h1 className="lg:text-xl text-lg text-ebony-50 my-1 first-letter:uppercase font-semibold">
-            Hosting
-          </h1>
-          <div className="lg:text-sm text-xs text-neutral-500 flex flex-col gap-2">
-            <a href="#">Why Host</a>
-            <a href="#">Hospitality</a>
-            <a href="#">Responsible Hosting</a>
-            <a href="#">Home Safety</a>
-          </div>
+          <button onClick={() => setShowfooter(!showFooter)} className=" h-8 w-8 grid place-items-center border rounded-full">
+            <KeyboardArrowDown />
+          </button>
         </div>
       </div>
+      {showFooter && (
+        <div className="py-5 px-1 lg:px-8  font-thin lg:grid lg:grid-cols-4 grid-cols-2 lg:gap-5 gap-8 place-items-center flex flex-row flex-wrap">
+          <div>
+            <h1 className=" text-lg my-1 font-semibold text-totem-pole-600">
+              Bookify
+            </h1>
+            <div className=" lg:text-sm text-xs  text-neutral-500 flex flex-col gap-2 overflow-ellipsis">
+              <p>
+                <strong className=" text-ebony-50">Address: </strong> 123
+                street,Embakasi, KE, 12345 KENYA
+              </p>
+              <p>
+                <strong className=" text-ebony-50">Phone: </strong>+254 792 8174
+                28
+              </p>
+              <p>
+                <strong className=" text-ebony-50">Email: </strong>
+                wallicestenewaweru@gmail.com
+              </p>
+            </div>
+          </div>
+          <div>
+            <h1 className="  text-lg text-ebony-50 my-1 first-letter:uppercase font-semibold">
+              Company
+            </h1>
+            <div className=" lg:text-sm text-xs text-neutral-500 flex flex-col gap-2">
+              <a href="#">About</a>
+              <a href="#">Careers</a>
+              <a href="#">Blog</a>
+              <a href="#">Terms & Policy</a>
+            </div>
+          </div>
+          <div>
+            <h1 className=" text-lg text-ebony-50 my-1 first-letter:uppercase font-semibold">
+              Discover
+            </h1>
+            <div className="lg:text-sm text-xs text-neutral-500 flex flex-col gap-2">
+              <a href="#">Trust & Safety</a>
+              <a href="#">Travel Credit</a>
+              <a href="#">Gift Cards</a>
+              <a href="#">Bookify Picks</a>
+            </div>
+          </div>
+          <div>
+            <h1 className="lg:text-xl text-lg text-ebony-50 my-1 first-letter:uppercase font-semibold">
+              Hosting
+            </h1>
+            <div className="lg:text-sm text-xs text-neutral-500 flex flex-col gap-2">
+              <a href="#">Why Host</a>
+              <a href="#">Hospitality</a>
+              <a href="#">Responsible Hosting</a>
+              <a href="#">Home Safety</a>
+            </div>
+          </div>
+        </div>
+      )}
     </footer>
   );
 };
