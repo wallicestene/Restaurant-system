aca0d7f9479e89587daba6064438e6a08b691d25
Your Name <wallicestenewaweru@gmail.com>
2024-02-12 15:37:18 +0300

    fonts update

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -40,7 +40,7 @@ const Profile = () => {
         </div>
         <div className="w-full p-5">
           <div className=" mt-5">
-            <h1 className="text-5xl font-bold leading-tight tracking-tight">
+            <h1 className="text-3xl font-bold leading-tight tracking-tight">
               Hello, {`${user?.first_name} ${user?.last_name}`}
             </h1>
           </div>
@@ -50,7 +50,7 @@ const Profile = () => {
           <div>
             <button
             onClick={LogOutUser}
-              className="inline-flex items-center justify-center h-12 px-10 py-0 text-xl font-semibold text-center no-underline align-middle transition-all duration-300 ease-in-out bg-transparent border-2 border-black border-solid rounded-lg cursor-pointer select-none hover:text-white hover:bg-black w-full hover:border-white focus:shadow-xs focus:no-underline"
+              className="inline-flex text-lg items-center justify-center h-12 px-10 py-0 font-semibold text-center no-underline align-middle transition-all duration-300 ease-in-out bg-transparent border-2 border-black border-solid rounded-lg cursor-pointer select-none hover:text-white hover:bg-black w-full hover:border-white focus:shadow-xs focus:no-underline"
             >Log Out
             </button>
           </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",
-  "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
-],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+  ],
   theme: {
     extend: {
       colors: {
@@ -20,22 +22,31 @@ export default {
           950: "#491301",
         },
       },
-      fontFamily:{
-        mulish: ['Mulish', "sans-serif"],
-        poppins:['Poppins','sans-serif'],
-        Montserrat: ["Montserrat", "sans-serif"]
+      fontFamily: {
+        mulish: ["Mulish", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        Montserrat: ["Montserrat", "sans-serif"],
       },
-      flex:{
-        "8": 0.8,
-        "2" : 0.2
+      flex: {
+        8: 0.8,
+        2: 0.2,
       },
       backgroundImage: {
-        'login': "url('./src/assets/loginFormImages/restaurant1.png')",
-        'signup': "url('./src/assets/loginFormImages/restaurant2.png')",
-      }
+        login: "url('./src/assets/loginFormImages/restaurant1.png')",
+        signup: "url('./src/assets/loginFormImages/restaurant2.png')",
+      },
+      fontSize: {
+        sm: "clamp(0.8rem, 0.17vw + 0.76rem, 0.89rem)",
+        base: "clamp(1rem, 0.34vw + 0.91rem, 1.19rem)",
+        lg: "clamp(1.2rem, 0.61vw + 0.97rem, 1.3rem)",
+        xl: "clamp(1.56rem, 1vw + 1.31rem, 2.11rem)",
+        "2xl": "clamp(1.95rem, 1.56vw + 1.56rem, 2.81rem)",
+        "3xl": "clamp(2.44rem, 2.38vw + 1.85rem, 3.75rem)",
+        "4xl": "clamp(3.05rem, 3.54vw + 2.17rem, 5rem)",
+        "5xl": "clamp(3.81rem, 5.18vw + 2.52rem, 6.66rem)",
+        "6xl": "clamp(4.77rem, 7.48vw + 2.9rem, 8.88rem)",
+      },
     },
   },
-  plugins: [
-    require('tailwind-clip-path'),
-  ],
+  plugins: [require("tailwind-clip-path")],
 };
